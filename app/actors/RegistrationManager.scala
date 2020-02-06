5049639d5ec535ea599d8f7bc1d812ca89db83e8
Maftunbek <prince777_98@mail.ru>
2020-02-06 15:54:46 +0500

    protocol and manager is added

--- app/actors/RegistrationManager.scala
+++ app/actors/RegistrationManager.scala
@@ -1,10 +1,13 @@
 package actors
 
 import akka.actor.Actor
+import akka.pattern.pipe
 import akka.util.Timeout
 import com.typesafe.scalalogging.LazyLogging
 import javax.inject.Inject
 import play.api.{Configuration, Environment}
+import protocols.RegistrationProtocol._
+import akka.actor.{Actor, ActorLogging}
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.DurationInt
@@ -18,10 +21,21 @@ class RegistrationManager @Inject()(val environment: Environment,
   implicit val defaultTimeout: Timeout = Timeout(60.seconds)
 
   def receive = {
-
-
+//    case AddRegistration(data) =>
+//      addRegistration(data).pipeTo(sender())
+//
+//    case GetRegistrationList =>
+//      getRegistrationList.pipeTo(sender())
+//
     case _ => logger.info(s"received unknown message")
   }
-
+//
+//  private def addRegistration(data: Registration) = {
+//
+//  }
+//
+//  private def getRegistrationList = {
+//
+//  }
 
 }
--- app/protocols/RegistrationProtocol.scala
+++ app/protocols/RegistrationProtocol.scala
@@ -1,7 +1,24 @@
 package protocols
 
+import java.time.Period
+
 import play.api.libs.json.{Json, OFormat}
 
 object RegistrationProtocol {
+
+  case object GetRegistrationList
+
+  case class AddRegistration(data: Registration)
+
+  case class Registration(id: Option[Int] = None,
+                          firsName: String,
+                          surName: String,
+                          lastName: String,
+                          phone: String,
+                          createdDate: String,
+                          period: Period)
+
+  implicit val registrationFormat: OFormat[Registration] = Json.format[Registration]
+
 
 }
