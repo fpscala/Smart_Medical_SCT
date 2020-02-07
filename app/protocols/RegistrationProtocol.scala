943247e3a5a204bc7c2c63bbceab13bff2e63b7f
Maftunbek <maftunbek.r@gmail.com>
2020-02-07 11:03:56 +0500

    #7 case classes is added

--- app/protocols/RegistrationProtocol.scala
+++ app/protocols/RegistrationProtocol.scala
@@ -1,24 +1,65 @@
 package protocols
 
-import java.time.Period
+import java.util.Date
 
-import play.api.libs.json.{Json, OFormat}
+import play.api.libs.json.{JsValue, Json, OFormat}
 
 object RegistrationProtocol {
 
   case object GetRegistrationList
 
-  case class AddRegistration(data: Registration)
+  case class AddRegistration(data: Patient)
 
-  case class Registration(id: Option[Int] = None,
+  case class Patient(id: Option[Int] = None,
                           firsName: String,
-                          surName: String,
+                          middleName: String,
                           lastName: String,
-                          phone: String,
-                          createdDate: String,
-                          period: Period)
+                          passport_sn: String,
+                          gender: String,
+                          birthday: String,
+                          address: String,
+                          phone: Option[String],
+                          cardNumber: String,
+                          profession: Option[String],
+                          workTypeId: Option[Int],
+                          lastCheckup: Option[Date],
+                          photo: Option[String],
+                          organizationId: Option[Int])
 
-  implicit val registrationFormat: OFormat[Registration] = Json.format[Registration]
+  implicit val registrationFormat: OFormat[Patient] = Json.format[Patient]
 
+  case class Organization(id: Option[Int] = None,
+                          organizationName: String)
+
+  implicit val organizationFormat: OFormat[Organization] = Json.format[Organization]
+
+  case class WorkType(id: Option[Int] = None,
+                      workType: String)
+
+  implicit val workTypeFormat: OFormat[WorkType] = Json.format[WorkType]
+
+  case class Laboratory(id: Option[Int] = None,
+                        laboratoryType: String)
+
+  implicit val laboratoryFormat: OFormat[Laboratory] = Json.format[Laboratory]
+
+  case class CheckupType(id: Option[Int] = None,
+                         checkupType: String)
+
+  implicit val checkupTypeFormat: OFormat[CheckupType] = Json.format[CheckupType]
+
+  case class DoctorType(id: Option[Int] = None,
+                        doctorType: String)
+
+  implicit val doctorTypeFormat: OFormat[DoctorType] = Json.format[DoctorType]
+
+  case class CheckupPeriod(id: Option[Int] = None,
+                            numberPerYear: Int,
+                            doctorTypeId: JsValue,
+                            labTypeId: JsValue,
+                            workTypeId: Int
+                           )
+
+  implicit val CheckupPeriodFormat: OFormat[CheckupPeriod] = Json.format[CheckupPeriod]
 
 }
